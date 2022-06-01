d2648ba1155e2464fb63245e0841170649b874dc
Dams-d <damien.duro8@gmail.com>
2022-06-01 10:13:01 +0200

    Fix Segment writeKey setup

--- src/components/SettingsEdit.vue
+++ src/components/SettingsEdit.vue
@@ -1,36 +1,36 @@
 <template>
-    <div class="segment-settings-edit">
-        <wwEditorFormRow required label="Write key">
-            <template #append-label>
-                <a
-                    class="segment-settings-edit__link"
-                    href="https://segment.com/docs/connections/find-writekey"
-                    target="_blank"
-                >
-                    Find it here
-                </a>
-            </template>
-            <wwEditorInputText
-                type="text"
-                name="api-key"
-                placeholder="Enter the API key"
-                :model-value="settings.publicData.writeKey"
-                large
-                @update:modelValue="changeWriteKey"
-            />
-        </wwEditorFormRow>
-    </div>
+    <wwEditorFormRow required label="Write key">
+        <template #append-label>
+            <a
+                class="segment-settings-edit__link"
+                href="https://segment.com/docs/connections/find-writekey"
+                target="_blank"
+            >
+                Find it here
+            </a>
+        </template>
+        <wwEditorInputText
+            type="text"
+            name="api-key"
+            placeholder="Enter the API key"
+            :model-value="settings.publicData.writeKey"
+            large
+            @update:modelValue="changeWriteKey"
+        />
+    </wwEditorFormRow>
 </template>
 
 <script>
 export default {
     props: {
+        plugin: { type: Object, required: true },
         settings: { type: Object, required: true },
     },
     emits: ['update:settings'],
     methods: {
         changeWriteKey(writeKey) {
             this.$emit('update:settings', { ...this.settings, publicData: { writeKey } });
+            this.plugin.load(writeKey);
         },
     },
 };
@@ -42,13 +42,6 @@ export default {
     flex-direction: column;
     &__link {
         color: var(--ww-color-blue-500);
-        margin-left: var(--ww-spacing-02);
-    }
-    &__row {
-        display: flex;
-        align-items: center;
-    }
-    &__radio-label {
         margin-left: var(--ww-spacing-02);
     }
 }
