2ec528c9aaea501d2c0fb9d9f81ad17f27021a87
Léo Coletta <mail.leo.coletta@gmail.com>
2025-01-14 22:25:48 +0100

    feat: add copilot configuration

--- ww-config.js
+++ ww-config.js
@@ -14,6 +14,22 @@ export default {
             code: 'identify',
             /* wwEditor:start */
             edit: () => import('./src/components/Identify.vue'),
+            copilot: {
+                description: 'Identify a user with specific traits',
+                returns: 'void',
+                schema: {
+                    userId: {
+                        type: 'string',
+                        description: 'Unique identifier for the user',
+                        bindable: true
+                    },
+                    traits: {
+                        type: 'array',
+                        description: 'Array of key-value pairs describing user traits',
+                        bindable: true
+                    }
+                }
+            },
             /* wwEditor:end */
         },
         {
@@ -24,6 +40,22 @@ export default {
             getIsValid({ event }) {
                 return !!event;
             },
+            copilot: {
+                description: 'Track a specific event with properties',
+                returns: 'void',
+                schema: {
+                    event: {
+                        type: 'string',
+                        description: 'Name of the event to track',
+                        bindable: true
+                    },
+                    properties: {
+                        type: 'array',
+                        description: 'Array of key-value pairs for event properties',
+                        bindable: true
+                    }
+                }
+            },
             /* wwEditor:end */
         },
         {
@@ -31,6 +63,27 @@ export default {
             code: 'page',
             /* wwEditor:start */
             edit: () => import('./src/components/Page.vue'),
+            copilot: {
+                description: 'Track a page view with optional category and properties',
+                returns: 'void',
+                schema: {
+                    category: {
+                        type: 'string',
+                        description: 'Category of the page',
+                        bindable: true
+                    },
+                    name: {
+                        type: 'string',
+                        description: 'Name of the page',
+                        bindable: true
+                    },
+                    properties: {
+                        type: 'array',
+                        description: 'Array of key-value pairs for page properties',
+                        bindable: true
+                    }
+                }
+            },
             /* wwEditor:end */
         },
         {
@@ -38,6 +91,22 @@ export default {
             code: 'screen',
             /* wwEditor:start */
             edit: () => import('./src/components/Screen.vue'),
+            copilot: {
+                description: 'Track a screen view in a mobile app',
+                returns: 'void',
+                schema: {
+                    name: {
+                        type: 'string',
+                        description: 'Name of the screen',
+                        bindable: true
+                    },
+                    properties: {
+                        type: 'array',
+                        description: 'Array of key-value pairs for screen properties',
+                        bindable: true
+                    }
+                }
+            },
             /* wwEditor:end */
         },
         {
@@ -48,6 +117,22 @@ export default {
             getIsValid({ groupId }) {
                 return !!groupId;
             },
+            copilot: {
+                description: 'Associate a user with a group',
+                returns: 'void',
+                schema: {
+                    groupId: {
+                        type: 'string',
+                        description: 'Unique identifier for the group',
+                        bindable: true
+                    },
+                    traits: {
+                        type: 'array',
+                        description: 'Array of key-value pairs describing group traits',
+                        bindable: true
+                    }
+                }
+            },
             /* wwEditor:end */
         },
         {
@@ -57,6 +142,22 @@ export default {
             edit: () => import('./src/components/Alias.vue'),
             getIsValid({ previousId }) {
                 return !!previousId;
+            },
+            copilot: {
+                description: 'Associate one user ID with another',
+                returns: 'void',
+                schema: {
+                    previousId: {
+                        type: 'string',
+                        description: 'Previous identifier for the user',
+                        bindable: true
+                    },
+                    userId: {
+                        type: 'string',
+                        description: 'New identifier for the user',
+                        bindable: true
+                    }
+                }
             },
             /* wwEditor:end */
         },
